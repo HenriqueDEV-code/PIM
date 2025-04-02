1a8cfe970d69ddeacd0bd6c4503fe97987989dd2
Luís Henrique Nascimento Belotti <pobsg.oficialbr@gmail.com>
2025-04-02 18:23:32 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoFaculdade/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ProjetoFaculdade
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Support_Titulo_Login = new System.Windows.Forms.Label();
+            this.label_Tech_Titulo_Login = new System.Windows.Forms.Label();
+            this.label_Car_Titulo_Login = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.CBX_LembrarSenha = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tB_Senha_Usuario = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Esqueceu_Senha = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tB_Usuario_Login = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_Support_Titulo_Login
+            // 
+            this.label_Support_Titulo_Login.AutoSize = true;
+            this.label_Support_Titulo_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F);
+            this.label_Support_Titulo_Login.ForeColor = System.Drawing.Color.White;
+            this.label_Support_Titulo_Login.Location = new System.Drawing.Point(460, 22);
+            this.label_Support_Titulo_Login.Name = "label_Support_Titulo_Login";
+            this.label_Support_Titulo_Login.Size = new System.Drawing.Size(187, 54);
+            this.label_Support_Titulo_Login.TabIndex = 9;
+            this.label_Support_Titulo_Login.Text = "Support";
+            this.label_Support_Titulo_Login.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label_Tech_Titulo_Login
+            // 
+            this.label_Tech_Titulo_Login.AutoSize = true;
+            this.label_Tech_Titulo_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F);
+            this.label_Tech_Titulo_Login.ForeColor = System.Drawing.Color.Yellow;
+            this.label_Tech_Titulo_Login.Location = new System.Drawing.Point(339, 22);
+            this.label_Tech_Titulo_Login.Name = "label_Tech_Titulo_Login";
+            this.label_Tech_Titulo_Login.Size = new System.Drawing.Size(128, 54);
+            this.label_Tech_Titulo_Login.TabIndex = 8;
+            this.label_Tech_Titulo_Login.Text = "Tech";
+            this.label_Tech_Titulo_Login.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label_Car_Titulo_Login
+            // 
+            this.label_Car_Titulo_Login.AutoSize = true;
+            this.label_Car_Titulo_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F);
+            this.label_Car_Titulo_Login.ForeColor = System.Drawing.Color.White;
+            this.label_Car_Titulo_Login.Location = new System.Drawing.Point(245, 22);
+            this.label_Car_Titulo_Login.Name = "label_Car_Titulo_Login";
+            this.label_Car_Titulo_Login.Size = new System.Drawing.Size(99, 54);
+            this.label_Car_Titulo_Login.TabIndex = 7;
+            this.label_Car_Titulo_Login.Text = "Car";
+            this.label_Car_Titulo_Login.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = false;
+            this.metroButton1.Location = new System.Drawing.Point(140, 514);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(245, 63);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton1.StyleManager = null;
+            this.metroButton1.TabIndex = 16;
+            this.metroButton1.Text = "Cadastrar";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = false;
+            this.metroButton2.Location = new System.Drawing.Point(500, 514);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(245, 63);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton2.StyleManager = null;
+            this.metroButton2.TabIndex = 17;
+            this.metroButton2.Text = "Login";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // CBX_LembrarSenha
+            // 
+            this.CBX_LembrarSenha.AutoSize = true;
+            this.CBX_LembrarSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.CBX_LembrarSenha.ForeColor = System.Drawing.Color.White;
+            this.CBX_LembrarSenha.Location = new System.Drawing.Point(140, 437);
+            this.CBX_LembrarSenha.Name = "CBX_LembrarSenha";
+            this.CBX_LembrarSenha.Size = new System.Drawing.Size(175, 29);
+            this.CBX_LembrarSenha.TabIndex = 13;
+            this.CBX_LembrarSenha.Text = "Lembrar senha";
+            this.CBX_LembrarSenha.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tB_Senha_Usuario);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(140, 333);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(605, 79);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // tB_Senha_Usuario
+            // 
+            this.tB_Senha_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.tB_Senha_Usuario.Location = new System.Drawing.Point(74, 17);
+            this.tB_Senha_Usuario.Name = "tB_Senha_Usuario";
+            this.tB_Senha_Usuario.PasswordChar = '*';
+            this.tB_Senha_Usuario.Size = new System.Drawing.Size(524, 53);
+            this.tB_Senha_Usuario.TabIndex = 14;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProjetoFaculdade.Properties.Resources.icons8_cadeado_2_60;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 57);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Esqueceu_Senha
+            // 
+            this.Esqueceu_Senha.AutoSize = true;
+            this.Esqueceu_Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.Esqueceu_Senha.ForeColor = System.Drawing.Color.White;
+            this.Esqueceu_Senha.Location = new System.Drawing.Point(542, 437);
+            this.Esqueceu_Senha.Name = "Esqueceu_Senha";
+            this.Esqueceu_Senha.Size = new System.Drawing.Size(203, 25);
+            this.Esqueceu_Senha.TabIndex = 14;
+            this.Esqueceu_Senha.Text = "Esqueceu a senha?";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tB_Usuario_Login);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(140, 238);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 79);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // tB_Usuario_Login
+            // 
+            this.tB_Usuario_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.tB_Usuario_Login.Location = new System.Drawing.Point(75, 17);
+            this.tB_Usuario_Login.Name = "tB_Usuario_Login";
+            this.tB_Usuario_Login.Size = new System.Drawing.Size(524, 53);
+            this.tB_Usuario_Login.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoFaculdade.Properties.Resources.icons8_usuário_60;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(370, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 54);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Login";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(19)))), ((int)(((byte)(30)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(884, 589);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Esqueceu_Senha);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CBX_LembrarSenha);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label_Support_Titulo_Login);
+            this.Controls.Add(this.label_Tech_Titulo_Login);
+            this.Controls.Add(this.label_Car_Titulo_Login);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 628);
+            this.MinimumSize = new System.Drawing.Size(900, 628);
+            this.Name = "FormLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_Support_Titulo_Login;
+        private System.Windows.Forms.Label label_Tech_Titulo_Login;
+        private System.Windows.Forms.Label label_Car_Titulo_Login;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private System.Windows.Forms.CheckBox CBX_LembrarSenha;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tB_Senha_Usuario;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Esqueceu_Senha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tB_Usuario_Login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProjetoFaculdade/FormCadastroFuncionarios.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.Design.WebControls.WebParts;
+using System.Windows.Forms;
+
+namespace ProjetoFaculdade
+{
+    public partial class FormCadastroFuncionarios : Form
+    {
+        public FormCadastroFuncionarios()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region TRATAMENTO DE ENTRADA DE DADOS EVENTO KEYPRESS
+        private void tB_NomeCompleto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsSeparator(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Permite a entrada
+            }
+        }
+
+        private void tB_Cargo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsSeparator(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Permite a entrada
+            }
+        }
+
+
+        #endregion
+
+
+
+
+        #region TRATAMENTO DE DADOS NULOS
+
+        private void tB_NomeCompleto_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tB_NomeCompleto.Text))
+            {
+                MessageBox.Show("Valor inválido. Campo não pode estar vazio!", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tB_NomeCompleto.Focus();
+            }
+
+        }
+
+        private void tB_Email_Leave(object sender, EventArgs e)
+        {
+            string[] dominiosPermitidos =
+            {
+                "@gmail.com", "@outlook.com", "@protonmail.com", "@zoho.com",
+                "@yahoo.com", "@icloud.com", "@gmx.com", "@aol.com",
+                "@fastmail.com", "@tutanota.com", "@gmail.com.br", "@outlook.com.br",
+                "@protonmail.com.br", "@zoho.com.br", "@yahoo.com.br", "@icloud.com.br",
+                "@gmx.com.br", "@aol.com.br", "@fastmail.com.br", "@tutanota.com.br",
+                "@hotmail.com"
+            };
+
+            string email = tB_Email.Text.Trim();
+
+            // Aqui vamos verificar se o e-mail tem um formato mínimo válido
+
+            if (!email.Contains("@") || email.Split('@').Length != 2)
+            {
+                MessageBox.Show("E-mail inválido!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tB_Email.Focus();
+                return;
+            }
+
+            // Obtem o dominio digitado pelo usuario
+            string dominio = "@" + email.Split('@')[1];
+
+            // Verifica se o dominio esta na lista permitida
+            bool dominioValido = Array.Exists(dominiosPermitidos, d => d.Equals(dominio, StringComparison.OrdinalIgnoreCase));
+
+            if (!dominioValido)
+            {
+                MessageBox.Show("Dominio de E-mail inválido!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tB_Email.Focus();
+            }
+        }
+
+        private void MtB_Nascimento_Leave(object sender, EventArgs e)
+        {
+            // vamos verificar se o campo esta vazio
+            if (string.IsNullOrWhiteSpace(MtB_Nascimento.Text))
+            {
+                return; // sai da funcao sem calcular nada
+            }
+
+            if (DateTime.TryParseExact(MtB_Nascimento.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dataNascimento))
+            {
+                // Calcular a idade
+                int idade = Calc_Idade(dataNascimento);
+
+                // Validação da idade permitida
+                if (idade <= 16 )
+                {
+                    MessageBox.Show("Idade não permitida, funcionário parece ser menor de idade! ",
+                        "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MtB_Nascimento.Clear();
+                    MtB_Nascimento.Focus();
+                } else if (idade >= 16 && idade < 18 || idade >= 60 && idade <= 80)
+                {
+                    MessageBox.Show("Idade não parece permitida! Gostaria de inserir mesmo assim? ", "Aviso", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    return;
+                }else if (idade >= 81)
+                {
+                    MessageBox.Show("Idade não permitida, funcionário parece que já aposentou! ",
+                     "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MtB_Nascimento.Clear();
+                    MtB_Nascimento.Focus();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Data inválida! Informe no formato dd/MM/yyyy.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MtB_Nascimento.Clear();
+                MtB_Nascimento.Focus();
+            }
+
+        }
+
+        // Calcular idade
+        private int Calc_Idade(DateTime DataNascimeto)
+        {
+            DateTime hoje = DateTime.Today;
+            int idade = hoje.Year - DataNascimeto.Year;
+
+            // Ajustar se ainda nao fez Aniverario este ano
+            if(DataNascimeto.Date > hoje.AddYears(-idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+
+
+        private void MtB_CPF_Leave(object sender, EventArgs e)
+        {
+            string cpf = new string(MtB_CPF.Text.Where(char.IsDigit).ToArray()); // Remove tudo exceto números
+
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                MessageBox.Show("Valor inválido. O campo não pode estar vazio!", "Aviso",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MtB_CPF.Focus();
+                return;
+            }
+
+            if (!ValidarCPF(cpf))
+            {
+                MessageBox.Show("CPF inválido! Digite um CPF válido.", "Erro",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MtB_CPF.Focus();
+            }
+
+
+        }
+
+        // Metodo para validar CPF
+        private bool ValidarCPF(string cpf)
+        {
+            if (cpf.Length != 11 || cpf.Distinct().Count() == 1)
+                return false; // Verifica se tem 11 dígitos e não são todos iguais
+
+            int[] multiplicador1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            int primeiroDigito = resto < 2 ? 0 : 11 - resto;
+
+            tempCpf += primeiroDigito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            int segundoDigito = resto < 2 ? 0 : 11 - resto;
+
+            return cpf.EndsWith($"{primeiroDigito}{segundoDigito}");
+
+        }
+
+        #endregion
+
+
+    }
+}
